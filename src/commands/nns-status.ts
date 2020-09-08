8613de6422bd02f534f94bfd3084c30b76a05d51
ShashankHegde16 <shashank16aug19@gmail.com>
2020-09-08 09:40:17 +0530

    added spec for jira api handler

--- src/commands/nns-status.ts
+++ src/commands/nns-status.ts
@@ -4,6 +4,10 @@ import Jira from '../service/jiraApiHandler';
 export class StatusCommand {
     readonly name: string = 'status';
     readonly alias: string = 'status';
+    readonly description: string = `Key in !nns.status <JIRATICKET> to get status`;
+    readonly man: string = `
+    Displays the status of the entered JIRA Ticket. 
+    `;
     private jiraApiHandler: Jira;
     constructor(jiraApiHandler?: Jira) {
         this.jiraApiHandler = jiraApiHandler || new Jira();
@@ -13,8 +17,8 @@ export class StatusCommand {
         helpEmbed.type = 'rich';
         if (args && args.length > 0) {
             args = args[0].replace(prefix, ''); /**Processes only the first word */
-            const cmdAsArg = commands.get(args);
             this.jiraApiHandler.getTicketStatus(args).then(status => {
+                console.log(status)
                 if (status && status.hasOwnProperty('errorMessages')) {
                     helpEmbed.setColor('#FF0000')
                         .setTitle(`Invalid Ticket or Permission is denied!`)
@@ -39,20 +43,28 @@ export class StatusCommand {
                     console.error(e);
                 }
                 return helpEmbed;
-            }).catch(e => console.error(e))
+            }).catch(e => {
+                console.error(e);
+
+            })
         }
-        helpEmbed.setColor('#F8AA2A')
-            .setTitle('nns-bot Status')
-            .setDescription('Supported Command')
-            .addField('!nns.status ABC-101', 'Displays the status of the entered JIRA Ticket.', true)
-            .setTimestamp();
-        try {
-            message.channel.send(helpEmbed);
-        } catch (e) {
-            console.error(e);
+        else {
+            helpEmbed.setColor('#F8AA2A')
+                .setTitle('nns-bot Status')
+                .setDescription('Supported Command')
+                .addField('!nns.status <<JiraTicketReference>>', 'Displays the status of the entered JIRA Ticket.', true)
+                .setTimestamp();
+            try {
+                message.channel.send(helpEmbed);
+            } catch (e) {
+                console.error(e);
+            }
+            return helpEmbed;
         }
-        return helpEmbed;
+
 
     }
+
+
 
 }
--- tests/commands/nns-status.spec.ts
+++ tests/commands/nns-status.spec.ts
@@ -2,38 +2,46 @@ import { config } from 'dotenv';
 config();
 import 'mocha';
 import { expect } from 'chai';
-import { Client, TextChannel, Message, MessageEmbed } from 'discord.js';
+import { Client, TextChannel, Message, MessageEmbed, NewsChannel } from 'discord.js';
 import { instance, mock, verify, when, spy } from 'ts-mockito';
 import { StatusCommand } from '../../src/commands';
+import sinon, { stubInterface } from "ts-sinon"
+import Jira from '../../src/service/jiraApiHandler';
+
 
 
 
 describe('StatusCommandHandler', () => {
     let mockedMessageClass: Message;
     let mockedMessageInstance: Message;
+    mockedMessageClass = mock(Message);
+    mockedMessageInstance = instance(mockedMessageClass);
     let client: Client;
     let command: StatusCommand;
+    let jira: Jira;
     let mckTextChannel;
     const commandsList = new Map();
     beforeEach(() => {
         client = new Client();
         client['prefix'] = process.env.PREFIX;
-        mockedMessageClass = mock(Message);
-        mockedMessageInstance = instance(mockedMessageClass);
+
+
         mckTextChannel = mock(TextChannel);
         mockedMessageInstance.channel = instance(mckTextChannel);
         command = new StatusCommand();
-        commandsList.set('test', { name: 'test', man: 'test' });
+        jira = new Jira();
+        commandsList.set('MO-49', { name: 'Test', man: 'Test' });
         client['commands'] = commandsList;
     });
     it('status without arguments', () => {
+
         mockedMessageInstance.content = '!nns.status';
         const helpEmbed = new MessageEmbed({
             type: 'rich',
             title: 'nns-bot Status',
             description: 'Supported Command',
             color: 16296490,
-            fields: [{ name: '!nns.status ABC-101', value: 'Displays the status of the entered JIRA Ticket.', inline: true }],
+            fields: [{ name: '!nns.status <<JiraTicketReference>>', value: 'Displays the status of the entered JIRA Ticket.', inline: true }],
             thumbnail: null,
             image: null,
             video: null,
@@ -49,5 +57,7 @@ describe('StatusCommandHandler', () => {
         delete helpEmbed.timestamp;
         expect(returnVal).to.deep.equal(helpEmbed);
     });
+
+  
 
 });
