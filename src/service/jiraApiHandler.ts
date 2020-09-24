583092748267a515ef4f8150a132efb667c036bc
ShashankHegde16 <shashank16aug19@gmail.com>
2020-09-24 20:00:00 +0530

    removed unused var

--- src/service/jiraApiHandler.ts
+++ src/service/jiraApiHandler.ts
@@ -1,16 +1,6 @@
 import * as request from 'request';
 
 export default class Jira {
-    private readonly host: string;
-    private readonly jiraAuth: string;
-
-
-    constructor() {
-        this.host = process.env.JIRA_HOST;
-        this.jiraAuth = process.env.JIRA_AUTH;
-
-    }
-
     public returnAwait(options, method) {
         return new Promise((resolved, rejected) => {
             request[method](options, (err, res, body) => {
